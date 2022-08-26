139e9e855cf593b852190e73cdb5dca9021014a5
Gledsonlucas111 <gledsonlucas111@gmail.com>
2022-08-26 17:59:19 -0300

    subindo projetos.json

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,13 +1,36 @@
-import Footer from "components/footer";
-import Header from "components/header";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+import { Content } from "./styles";
 
 const Projects = () => {
   return (
-  <div>
-    <Header />
-
-    <Footer />
-  </div>
+  <Content>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image=""
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  </Content>
   );
 };
 
---
+++ src/pages/projects/styles.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import Box from "@mui/material/Box";
+
+
+export const Content = styled(Box)`
+display: flex;
+`;
